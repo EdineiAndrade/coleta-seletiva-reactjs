f2b5e0a0df63213de3dfbe46a4972d08125a925f
EdineiAndrade <dinei22.ba@gmail.com>
2020-12-14 23:29:43 -0300

    PaginaInicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header/>
+      <h1>Hello Dev!!</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;
